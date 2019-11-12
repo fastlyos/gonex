9ffbd43f0fb7fb0c9acba30894254be3f2bc454d
Zergity <zergity@gmail.com>
2019-11-12 13:44:26 +0700

    Revert "core,params: keep flushing the snapshot block until CoLoa upgrade"
    This reverts commit c9a4a72dde715614ab1c12102a438a53c9949ed2.

--- params/config.go
+++ params/config.go
@@ -571,7 +571,7 @@ func (c *ChainConfig) IsThangLongPreparationBlock(num *big.Int) bool {
 
 // IsSnapshotBlock returns whether num represents a block number exactly at the snapshot block of an epoch.
 func (c *ChainConfig) IsSnapshotBlock(num *big.Int) bool {
-	if c.Dccs == nil || c.Dccs.ThangLongBlock == nil || c.IsCoLoa(num) {
+	if c.Dccs == nil || c.Dccs.ThangLongBlock == nil {
 		return false
 	}
 	forBlock := new(big.Int).SetUint64(CanonicalDepth)
